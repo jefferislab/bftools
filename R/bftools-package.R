80175129ae519f4902c96a534118468a5aa52637
Gregory Jefferis <jefferis@gmail.com>
2015-01-29 15:54:37 -0800

    skeleton package

---
+++ R/bftools-package.R
@@ -0,0 +1,9 @@
+#' Libraries for biological image input/output
+#' 
+#' See \url{http://loci.wisc.edu/software/bio-formats} for detailed description
+#' of the contents, licensing etc.
+#' @name bftools-package
+#' @aliases bftools
+#' @docType package
+#' @keywords package
+NULL
