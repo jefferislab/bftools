4397fa174b59e5826278918e6c7998101312cd1d
Gregory Jefferis <jefferis@gmail.com>
2019-11-03 18:47:55 +0000

    make test quiet

--- tests/testthat/test-showinf.R
+++ tests/testthat/test-showinf.R
@@ -1,5 +1,7 @@
 context("showinf")
 
 test_that("can run showinf without args to see help", {
-  expect_equal(showinf(), 1L)
+  # warning because showinf returns 1 
+  expect_warning(sihelp <- showinf(stderr=TRUE, stdout=TRUE))
+  expect_match(paste(sihelp, collapse=""), 'showinf file')
 })
